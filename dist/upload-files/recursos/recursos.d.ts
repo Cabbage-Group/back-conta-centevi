e4b78015e44a4f877f66b6fab29ed4f80601cb0d
root <gavadmalv@gmail.com>
2025-04-02 13:10:14 -0500

    eliminar dist

--- dist/upload-files/recursos/recursos.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const excelDateToJSDate: (serial: number) => Date;
-export declare const getDescription: (fechaString: string, referencia: string, source: string, descripcion: string) => string;
-export declare const generateGroupKey: (row: any, fields: string[]) => string;
-export declare const getGroupingFields: (source: string) => string[];
--- dist/upload-files/upload-files.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UploadFilesService } from './upload-files.service';
-export declare class UploadFilesController {
-    private readonly uploadFileService;
-    constructor(uploadFileService: UploadFilesService);
-    agruparFechaReferencia(file: Express.Multer.File): Promise<{
-        downloadLink: string;
-    }>;
-    downloadFile(filename: string, res: any): any;
-}
